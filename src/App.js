42565bab0e5b42f9ccafa1ae1c8f914e5c375b6c
coderashraf <ashrafulbnt@gmail.com>
2022-02-17 00:22:13 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+// import image1 from ".images/image1.jpg"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Library vs Framework</h1>
+     <img src="/images/image1.jpg" alt='image1'/>
+     <h1>Folder Structure of React App</h1>
+     <img src="/images/image2.jpg" alt='image2'/>
+    </div>
+  );
+}
+
+export default App;
